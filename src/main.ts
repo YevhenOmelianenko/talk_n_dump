618f52dae5d89c433f4a29861047d4fdd9adcd55
YevhenOmelianenko <yevhen.omelianenko1@gmail.com>
2025-09-15 00:05:54 +0300

    Still works on my machine5

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const sendBtn = document.getElementById("sendBtn");
 
   if (chatContainer && usernameInput && messageInput && sendBtn) {
-    setInterval(async () => {
+    const isAtDown = () => chatContainer.scrollTop === chatContainer.scrollHeight;
+    const scrollToDown = () => {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    };
+    const loadChat = async () => {
       const res = await fetch("http://46.101.114.148:3000/chat/messages");
       const json = await res.json();
       chatContainer.innerHTML = json
@@ -14,14 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="message-content">
             <div class="message-username">${messObj.username}</div>
             <div class="message-text">${messObj.message}</div>
-            <div class="message-time">12:35</div>
+            <div class="message-time"></div>
         </div>
     </div>`;
         })
         .join("\n");
-    }, 1000);
+      if (isAtDown()) {
+        scrollToDown();
+      }
+    };
+    loadChat();
+    setInterval(loadChat, 1000);
 
-    sendBtn.addEventListener("click", async () => {
+    const sendMessage = async () => {
+      if (!usernameInput.value || !messageInput.value) {
+        return;
+      }
       await fetch("http://46.101.114.148:3000/chat/message", {
         method: "POST",
         headers: {
@@ -33,6 +45,15 @@ document.addEventListener("DOMContentLoaded", () => {
           message: messageInput.value,
         }),
       });
+      messageInput.value = "";
+      messageInput.focus();
+    };
+
+    sendBtn.addEventListener("click", sendMessage);
+    messageInput.addEventListener("keyup", async (e) => {
+      if (e.key === "Enter") {
+        await sendMessage();
+      }
     });
   }
 });
