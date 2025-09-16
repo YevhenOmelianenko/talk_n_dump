ed75344d2d65be7f7c0da734fbf6fac7adc8c946
YevhenOmelianenko <yevhen.omelianenko1@gmail.com>
2025-09-16 23:31:53 +0300

    Still works on my machine7

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 declare const uuidv4: () => string;
 
+type MessageObject = {
+  id: string;
+  username: string;
+  message: string;
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
   const chatContainer = document.getElementById("chatContainer");
   const usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
@@ -7,6 +13,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   const sendBtn = document.getElementById("sendBtn");
 
   if (chatContainer && usernameInput && messageInput && sendBtn) {
+    const isOwnMessage = (mesObj: MessageObject) => {
+      const myIdsStr = localStorage.getItem("myIds");
+      const myIds = myIdsStr ? JSON.parse(myIdsStr) : [];
+      return myIds.includes(mesObj.id);
+    };
+
     const hideChatContainer = async () => {
       chatContainer.style.visibility = "hidden";
     };
@@ -22,14 +34,22 @@ document.addEventListener("DOMContentLoaded", async () => {
       const res = await fetch("http://46.101.114.148:3000/chat/messages");
       const json = await res.json();
       chatContainer.innerHTML = json
-        .map((messObj: any) => {
-          return `<div class="message">
-        <div class="message-content">
-            <div class="message-username">${messObj.username}</div>
-            <div class="message-text">${messObj.message}</div>
-            <div class="message-time"></div>
-        </div>
-    </div>`;
+        .map((messObj: MessageObject) => {
+          return isOwnMessage(messObj)
+            ? `<div class="message own-message">
+                  <div class="message-content">
+                      <div class="message-username">${messObj.username}</div>
+                      <div class="message-text">${messObj.message}</div>
+                      <div class="message-time"></div>
+                   </div>
+               </div>`
+            : `<div class="message">
+                  <div class="message-content">
+                      <div class="message-username">${messObj.username}</div>
+                      <div class="message-text">${messObj.message}</div>
+                      <div class="message-time"></div>
+                  </div>
+              </div>`;
         })
         .join("\n");
 
@@ -49,13 +69,19 @@ document.addEventListener("DOMContentLoaded", async () => {
       if (!usernameInput.value || !messageInput.value) {
         return;
       }
+      const id = uuidv4();
+      const myidsStr = localStorage.getItem("myIds");
+      const myIds = myidsStr ? JSON.parse(myidsStr) : [];
+      myIds.push(id);
+      localStorage.setItem("myIds", JSON.stringify(myIds));
+
       await fetch("http://46.101.114.148:3000/chat/message", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          id: uuidv4(),
+          id,
           username: usernameInput.value,
           message: messageInput.value,
         }),
