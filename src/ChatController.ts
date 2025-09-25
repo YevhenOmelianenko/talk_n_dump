b2b71914ce54525768004752fef2d7903d0289d3
YevhenOmelianenko <yevhen.omelianenko1@gmail.com>
2025-09-26 00:09:36 +0300

    Still works on my machine 10 !!!

--- src/ChatController.ts
+++ src/ChatController.ts
@@ -1,4 +1,5 @@
 import { ChatModel } from "./ChatModel";
+import { MessageObject } from "./ChatModel";
 import { ChatView } from "./ChatView";
 
 export class ChatController {
@@ -7,5 +8,39 @@ export class ChatController {
   constructor() {
     this.model = new ChatModel();
     this.view = new ChatView();
+
+    this.view.onReady(() => {
+      this.initialize();
+    });
   }
+  private initialize = async () => {
+    await this.view.hideChatContainer();
+    await this.loadChat();
+    await this.view.scrollToDown();
+    await this.view.showChatContainer();
+
+    this.view.onSendMessage((message: MessageObject) => {
+      this.model.sendMessage(message);
+    });
+
+    setInterval(this.loadChat, 1000);
+  };
+
+  private loadChat = async () => {
+    const messages = await this.model.loadMessages();
+    this.view.clearMessages();
+    messages.forEach((message) => {
+      if (this.model.isOwnMessage(message)) {
+        this.view.displayOwnMessage(message);
+      } else {
+        this.view.displayOtherMessage(message);
+      }
+    });
+
+    await this.view.waitAnimationFrame();
+
+    if (this.view.isAtDown()) {
+      this.view.scrollToDown();
+    }
+  };
 }
--- src/ChatView.ts
+++ src/ChatView.ts
@@ -1,3 +1,4 @@
+import { MessageObject } from "./ChatModel";
 export class ChatView {
   private chatContainer;
   private usernameInput;
@@ -14,10 +15,72 @@ export class ChatView {
     this.emojiBtn = document.getElementById("emojiBtn") as HTMLElement;
     this.emojiPicker = document.getElementById("emojiPicker") as HTMLElement;
   }
+
+  onReady = (func: () => void) => {
+    document.addEventListener("DOMContentLoaded", func);
+  };
+
   hideChatContainer = async () => {
     this.chatContainer.style.visibility = "hidden";
   };
   showChatContainer = async () => {
     this.chatContainer.style.visibility = "visible";
+  };
+
+  isAtDown = () => this.chatContainer.scrollTop === this.chatContainer.scrollHeight;
+  scrollToDown = async () => {
+    this.chatContainer.scrollTop = this.chatContainer.scrollHeight;
+  };
+
+  private sendMessage = async (func: (message: MessageObject) => void) => {
+    if (!this.usernameInput.value || !this.messageInput.value) {
+      return;
+    }
+    func({
+      username: this.usernameInput.value,
+      message: this.messageInput.value,
+    });
+    this.messageInput.value = "";
+    this.messageInput.focus();
+  };
+
+  onSendMessage = (func: (message: MessageObject) => void) => {
+    this.sendBtn.addEventListener("click", () => {
+      this.sendMessage(func);
+    });
+    this.messageInput.addEventListener("keyup", async (e) => {
+      if (e.key === "Enter") {
+        this.sendMessage(func);
+      }
+    });
+  };
+
+  clearMessages = () => {
+    this.chatContainer.innerHTML = "";
+  };
+
+  displayOwnMessage = (messObj: MessageObject) => {
+    this.chatContainer.innerHTML += `
+             <div class="message own-message">
+               <div class="message-content">
+                   <div class="message-username">${messObj.username}</div>
+                   <div class="message-text">${messObj.message}</div>
+                   <div class="message-time"></div>
+                </div>
+            </div>`;
+  };
+  displayOtherMessage = (messObj: MessageObject) => {
+    this.chatContainer.innerHTML += `
+             <div class="message own-message">
+               <div class="message-content">
+                   <div class="message-username">${messObj.username}</div>
+                   <div class="message-text">${messObj.message}</div>
+                   <div class="message-time"></div>
+                </div>
+            </div>`;
+  };
+
+  waitAnimationFrame = async () => {
+    await new Promise((resolve) => requestAnimationFrame(resolve));
   };
 }
